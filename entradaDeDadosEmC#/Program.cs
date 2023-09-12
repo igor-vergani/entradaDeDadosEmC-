c88f3d499af6d97c03145ba65083844a04b651c8
Igor Vergas <igor_vergani@hotmail.com>
2023-09-12 12:18:58 -0300

    Adicionar arquivos de projeto.

---
+++ entradaDeDadosEmC#/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Course
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite uma frase");
+            string frase = Console.ReadLine();
+
+            Console.WriteLine("Digite uma cor");
+            string x = Console.ReadLine();
+
+            Console.WriteLine("Digite outra cor");
+            string y = Console.ReadLine();
+
+            Console.WriteLine("mais uma cor");
+            string z = Console.ReadLine();
+
+            Console.WriteLine("digite 3 palavras");
+            string s = Console.ReadLine();
+            string[] vet = s.Split(' ');
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            
+
+       
+        }
+    }
+}
